ceb03bf939a8093a61e8ab2349927c9c58d5e04f
Neto test <lola5_575@rambler.ru>
2023-05-01 15:30:30 +0300

    Change Task 2

--- task_3/solution.js
+++ task_3/solution.js
@@ -2,6 +2,7 @@ function renderCartItem(item) {
     const product = item;
 
     // Задание №3.1. Формирование строки корзины
+    
 
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
